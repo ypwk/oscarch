f130a4ab8680956a5d12b803e1267b56c42f4548
Oscar Chaparro <ojcch1@gmail.com>
2021-02-17 14:51:56 -0500

    Base code for A2

---
+++ libclient/include/Lib.h
@@ -0,0 +1,17 @@
+//
+// Created by ojcch on 2/12/2021.
+//
+
+#ifndef LIBRARY_LIB_H
+#define LIBRARY_LIB_H
+
+
+class Lib {
+public:
+    static void algorithmA(const int& n);
+    static void algorithmB(const int& n);
+    static void algorithmC(const int& n);
+};
+
+
+#endif //LIBRARY_LIB_H
---
+++ libclient/src/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <Lib.h>
+
+using namespace std;
+
+int main() {
+    //you must change/expand this code
+    int n = 20;
+    Lib::algorithmA(n);
+    cout << "Successful execution" <<endl;
+    return 0;
+}
