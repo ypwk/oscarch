50607d8981a51f8588b26dfde29d79d316664480
Oscar Chaparro <ojcch1@gmail.com>
2021-03-15 20:57:44 -0400

    Minor change.

--- trees/build_run_unix.sh
+++ trees/build_run_unix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
 #create directory
-mkdir -p build-linux
+mkdir -p build-unix
 
 #go to directory
-cd build-linux
+cd build-unix
 
 #cmake will check if the compiler, C++ libraries, etc. are correct in your system and generate the necesary files to compile the code
 cmake ../
